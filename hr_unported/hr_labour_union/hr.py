bc5a9428a6c5b8ccb705f435b790b7a775567096
Usman Khan <usman.cs@gmail.com>
2015-07-31 16:16:26 +0500

    Added addon: hr_labour_union

---
+++ hr_unported/hr_labour_union/hr.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+#
+#
+#    Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+from openerp.osv import fields, orm
+from openerp import models, fields, api
+
+
+class hr_employee(orm.Model):
+
+    _name = 'hr.employee'
+    _inherit = 'hr.employee'
+       
+    is_labour_union = fields.Boolean(
+            'Labour Union Member'
+        )
+    labour_union_date = fields.Date(
+            'Date of Membership'
+        )
+
+
+class hr_contract(orm.Model):
+
+    _name = 'hr.contract'
+    _inherit = 'hr.contract'
+     
+    is_labour_union = fields.Boolean(
+            string='Labour Union Member',
+            related='employee_id.is_labour_union',
+            store=True
+        )
